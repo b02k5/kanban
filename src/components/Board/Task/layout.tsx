18dfeac6cc8ba1ed91fbd896eb8b18a973978caa
Bogdan Kochurov <kochurov.b@gmail.com>
2019-05-08 16:56:52 +0400

    create components for Task

--- src/components/Board/Task/layout.tsx
+++ src/components/Board/Task/layout.tsx
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import styled from "styled-components";
 import { TaskType } from "../../../store/types/tasks";
 import Modal from "../../Modal";
-import TextareaAutosize from 'react-textarea-autosize';
+import TextareaAutosize from "react-textarea-autosize";
 
 interface IProps {
   task: TaskType;
@@ -19,19 +19,19 @@ interface IProps {
 
 const BoardTask = styled.div`
   width: 100%;
-  padding: 10px 20px;
-  border: 1px solid #eaeff5;
-  border-radius: 8px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
+  padding: 35px 30px 15px;
+  border-radius: 5px;
+  background-color: white;
+  cursor: pointer;
 `;
 
-const BoardTaskName = styled.span`
-  color: #122144;
-  font-size: 15px;
-  line-height: 20px;
-  font-weight: 400;
+const BoardTaskName = styled.h3`
+  color: #212225;
+  font-size: 17px;
+  line-height: 22px;
+  font-weight: 500;
+  display: block;
+  margin: 0 0 15px 0;
 `;
 
 const ContainerStyles = styled.div`
@@ -45,9 +45,9 @@ const ContainerStyles = styled.div`
 `;
 
 const ModalTime = styled.time`
-  color: #a0aab0;
+  color: #9ba8b0;
   font-size: 12px;
-  margin-bottom: 5px;
+  margin-bottom: 3px;
   display: inline-block;
 `;
 const ModalTitle = styled.textarea`
@@ -82,6 +82,38 @@ const ModalDescription = styled.div`
   }
 `;
 
+const BoardTime = styled.time`
+  font-size: 11px;
+  line-height: 16px;
+`;
+
+const BoardDescription = styled.p`
+  color: #4e4f53;
+  font-size: 13px;
+  line-height: 18px;
+  margin: 0 0 30px 0;
+`;
+
+const BoardFooter = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+const BoardTagList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+const BoardTagItem = styled.li``;
+const BoardTag = styled.span`
+  color: white;
+  font-size: 13px;
+  line-height: 19px;
+  font-weight: 500;
+  background-color: #b7f5de;
+  padding: 2px 10px;
+  border-radius: 20px;
+`;
+
 export default ({
   task,
   onDrag,
@@ -90,42 +122,45 @@ export default ({
   isModalOpen,
   onTask
 }: IProps): JSX.Element => (
-    <Fragment>
-      <BoardTask
-        draggable={true}
-        onDragStart={e => onDrag(e, task, listId)}
-        onDragOver={onNoAllowDrop}
-        onClick={onTask}
-      >
-        <BoardTaskName>{task.name}</BoardTaskName>
-      </BoardTask>
+  <Fragment>
+    <BoardTask
+      draggable={true}
+      onDragStart={e => onDrag(e, task, listId)}
+      onDragOver={onNoAllowDrop}
+      onClick={onTask}
+    >
+      <BoardTime as={ModalTime}>03 July 2019</BoardTime>
+      <BoardTaskName>{task.name}</BoardTaskName>
+      <BoardDescription>
+        Fresh new onboarding, width 3 screens that should
+      </BoardDescription>
+      <BoardFooter>
+        <BoardTagList>
+          <BoardTagItem>
+            <BoardTag>UI</BoardTag>
+          </BoardTagItem>
+        </BoardTagList>
+      </BoardFooter>
+    </BoardTask>
 
+    {isModalOpen && (
       <Modal modalClick={onTask} containerStyles={ContainerStyles}>
         <ModalTime>04:00 PM</ModalTime>
         <ModalTitle>{task.name}</ModalTitle>
         <ModalDescription>
-          <TextareaAutosize style={{
-            border: 0, color: '#122144',
-            fontSize: '14px',
-            lineHeight: '19px',
-            resize: 'none',
-            width: '100%',
-            outline: 'none'
-          }} />
+          <TextareaAutosize
+            style={{
+              border: 0,
+              color: "#122144",
+              fontSize: "14px",
+              lineHeight: "19px",
+              resize: "none",
+              width: "100%",
+              outline: "none"
+            }}
+          />
         </ModalDescription>
       </Modal>
-
-      {/* {isModalOpen && (
-      <Modal modalClick={onTask}>
-        <ModalContainer>
-          <ModalWrapper>
-            <ModalHeader>
-              <ModalTime>123</ModalTime>
-              <ModalTitle>{task.name}</ModalTitle>
-            </ModalHeader>
-          </ModalWrapper>
-        </ModalContainer>
-      </Modal>
-    )} */}
-    </Fragment>
-  );
+    )}
+  </Fragment>
+);
