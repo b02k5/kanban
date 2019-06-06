9800e4418f3bf64f8ecaebc57e77d34485a10968
Bogdan Kochurov <kochurov.b@gmail.com>
2019-06-06 16:15:25 +0400

    change onDragEnd position for App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -22,61 +22,63 @@ interface IDispatchToProps {
 
 type Props = IDispatchToProps;
 
-const onDragEnd = (result: DropResult, props: Props) => {
-  const { source, destination, draggableId, type } = result;
-  const { moveTask, changePositionTasks, changePositionList } = props;
-  if (!destination) {
-    return;
-  }
+const App: React.FunctionComponent<Props> = props => {
+  const onDragEnd = (result: DropResult, props: Props) => {
+    const { source, destination, draggableId, type } = result;
+    const { moveTask, changePositionTasks, changePositionList } = props;
+    if (!destination) {
+      return;
+    }
 
-  if (
-    destination.droppableId === source.droppableId &&
-    destination.index === source.index
-  ) {
-    return;
-  }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
 
-  if (type === "list") {
-    const changePositionListArgs = {
-      boardId: Number(source.droppableId),
-      listId: Number(draggableId),
-      sourceIndex: source.index,
-      destinationIndex: destination.index
-    };
-    changePositionList(changePositionListArgs);
-    return;
-  }
+    if (type === "list") {
+      const changePositionListArgs = {
+        boardId: Number(source.droppableId),
+        listId: Number(draggableId),
+        sourceIndex: source.index,
+        destinationIndex: destination.index
+      };
+      changePositionList(changePositionListArgs);
+      return;
+    }
 
-  if (source.droppableId === destination.droppableId) {
-    const changePositionTasksArgs = {
-      listId: Number(source.droppableId),
-      taskId: Number(draggableId),
-      sourceIndex: source.index,
-      destinationIndex: destination.index
-    };
-    changePositionTasks(changePositionTasksArgs);
-    return;
-  } else {
-    const moveTaskArgs = {
-      sourceListId: Number(source.droppableId),
-      taskId: Number(draggableId),
-      targetListId: Number(destination.droppableId),
-      destinationIndex: destination.index
-    };
-    moveTask(moveTaskArgs);
-  }
+    if (source.droppableId === destination.droppableId) {
+      const changePositionTasksArgs = {
+        listId: Number(source.droppableId),
+        taskId: Number(draggableId),
+        sourceIndex: source.index,
+        destinationIndex: destination.index
+      };
+      changePositionTasks(changePositionTasksArgs);
+      return;
+    } else {
+      const moveTaskArgs = {
+        sourceListId: Number(source.droppableId),
+        taskId: Number(draggableId),
+        targetListId: Number(destination.droppableId),
+        destinationIndex: destination.index
+      };
+      moveTask(moveTaskArgs);
+    }
+  };
+
+  return (
+    <DragDropContext onDragEnd={result => onDragEnd(result, props)}>
+      <div className="App">
+        <Switch>
+          <Route path="/" component={Main} exact />
+          <Route path="/board/:id" component={Board} />
+        </Switch>
+      </div>
+    </DragDropContext>
+  );
 };
-
-const App: React.FunctionComponent<Props> = props => (
-  <DragDropContext onDragEnd={result => onDragEnd(result, props)}>
-    <div className="App">
-      <Switch>
-        <Route path="/" component={Main} exact />
-        <Route path="/board/:id" component={Board} />
-      </Switch>
-    </div>
-  </DragDropContext>
-);
 
 const mapDispatchToProps: IDispatchToProps = {
   changePositionTasks,
