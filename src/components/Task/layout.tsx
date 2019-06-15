ed57168a565db450184ab740607632a28e0870cd
Bogdan Kochurov <kochurov.b@gmail.com>
2019-06-15 11:56:03 +0400

    create styles for Task component

--- src/components/Task/layout.tsx
+++ src/components/Task/layout.tsx
@@ -1,10 +1,11 @@
-import React, { Fragment } from "react";
-import styled from "styled-components";
+import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import Truncate from "react-truncate";
 
 import { TaskType } from "../../store/types/tasks";
 import TaskDetails from "../Modal/TaskDetails";
+
+import * as TaskLayout from "./styles";
 
 interface IProps {
   task: TaskType;
@@ -13,66 +14,11 @@ interface IProps {
   onModalToggle: () => void;
 }
 
-const Task = styled.div<{ isDragging: boolean }>`
-  width: 100%;
-  padding: 15px 20px;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-bottom: 10px;
-  transition: background-color 0.15s ease;
-  background-color: ${props => (props.isDragging ? "#67686d" : "white")};
-`;
-
-const Name = styled.h3<{ isDragging: boolean }>`
-  color: ${props => (props.isDragging ? "white" : "#212225")};
-  font-size: 17px;
-  line-height: 22px;
-  font-weight: 500;
-  display: block;
-  margin: 0;
-`;
-
-const Time = styled.time`
-  display: block;
-  color: #a4afb6;
-  font-size: 11px;
-  line-height: 16px;
-  margin-bottom: 1px;
-`;
-
-const Description = styled.p<{ isDragging: boolean }>`
-  color: ${props => (props.isDragging ? "white" : "#4e4f53")};
-  font-size: 13px;
-  line-height: 18px;
-  margin: 10px 0 0 0;
-`;
-
-const Footer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  margin-top: 15px;
-`;
-const TagLists = styled.ul`
-  list-style: none;
-  padding: 0;
-`;
-const TagItem = styled.li``;
-const Tag = styled.span`
-  color: white;
-  font-size: 13px;
-  line-height: 19px;
-  font-weight: 500;
-  background-color: #b7f5de;
-  padding: 2px 10px;
-  border-radius: 20px;
-`;
-
 export default ({ task, isModalOpen, onModalToggle, index }: IProps) => (
-  <Fragment>
+  <>
     <Draggable draggableId={`${task.id}`} index={index}>
       {(provided, snapshot) => (
-        <Task
+        <TaskLayout.Main
           draggable={true}
           onClick={onModalToggle}
           {...provided.draggableProps}
@@ -80,23 +26,25 @@ export default ({ task, isModalOpen, onModalToggle, index }: IProps) => (
           ref={provided.innerRef}
           isDragging={snapshot.isDragging}
         >
-          <Time>{task.date}</Time>
-          <Name isDragging={snapshot.isDragging}>{task.name}</Name>
-          <Description isDragging={snapshot.isDragging}>
+          <TaskLayout.Time>{task.date}</TaskLayout.Time>
+          <TaskLayout.Name isDragging={snapshot.isDragging}>
+            {task.name}
+          </TaskLayout.Name>
+          <TaskLayout.Description isDragging={snapshot.isDragging}>
             <Truncate lines={4} ellipsis="..." width={210}>
               {task.description}
             </Truncate>
-          </Description>
-          <Footer>
-            <TagLists>
-              <TagItem>
-                <Tag>UI</Tag>
-              </TagItem>
-            </TagLists>
-          </Footer>
-        </Task>
+          </TaskLayout.Description>
+          <TaskLayout.Footer>
+            <TaskLayout.TagLists>
+              <TaskLayout.TagItem>
+                <TaskLayout.Tag>UI</TaskLayout.Tag>
+              </TaskLayout.TagItem>
+            </TaskLayout.TagLists>
+          </TaskLayout.Footer>
+        </TaskLayout.Main>
       )}
     </Draggable>
     {isModalOpen && <TaskDetails task={task} modalClick={onModalToggle} />}
-  </Fragment>
+  </>
 );
---
+++ src/components/Task/styles.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const Main = styled.div<{ isDragging: boolean }>`
+  width: 100%;
+  padding: 15px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-bottom: 10px;
+  transition: background-color 0.15s ease;
+  background-color: ${props => (props.isDragging ? "#67686d" : "white")};
+`;
+
+export const Name = styled.h3<{ isDragging: boolean }>`
+  color: ${props => (props.isDragging ? "white" : "#212225")};
+  font-size: 17px;
+  line-height: 22px;
+  font-weight: 500;
+  display: block;
+  margin: 0;
+`;
+
+export const Time = styled.time`
+  display: block;
+  color: #a4afb6;
+  font-size: 11px;
+  line-height: 16px;
+  margin-bottom: 1px;
+`;
+
+export const Description = styled.p<{ isDragging: boolean }>`
+  color: ${props => (props.isDragging ? "white" : "#4e4f53")};
+  font-size: 13px;
+  line-height: 18px;
+  margin: 10px 0 0 0;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin-top: 15px;
+`;
+
+export const TagLists = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+export const TagItem = styled.li``;
+
+export const Tag = styled.span`
+  color: white;
+  font-size: 13px;
+  line-height: 19px;
+  font-weight: 500;
+  background-color: #b7f5de;
+  padding: 2px 10px;
+  border-radius: 20px;
+`;
