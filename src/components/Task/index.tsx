3ae5eb7e0897b15a8f58422415c71281abe29edc
Bogdan Kochurov <kochurov.b@gmail.com>
2019-06-15 22:00:15 +0400

    remove connect from Task

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,27 +1,18 @@
 import React, { useState } from "react";
 import { Draggable } from "react-beautiful-dnd";
 import Truncate from "react-truncate";
-import { connect } from "react-redux";
 
-import { TaskArguments } from "../../store/types/tasks";
-import { addTask } from "../../store/actions/tasks";
 import { TaskType } from "../../store/types/tasks";
 import TaskDetails from "../Modal/TaskDetails";
 
-import * as TaskLayout from "./styles";
-
-interface IDispatchProps {
-  addTask: ({  }: TaskArguments) => void;
-}
+import * as Task from "./styles";
 
 interface IProps {
   task: TaskType;
   index: number;
 }
 
-type Props = IDispatchProps & IProps;
-
-const Task: React.FunctionComponent<Props> = props => {
+export default (props: IProps) => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const { task, index } = { ...props };
 
@@ -29,7 +20,7 @@ const Task: React.FunctionComponent<Props> = props => {
     <>
       <Draggable draggableId={`${task.id}`} index={index}>
         {(provided, snapshot) => (
-          <TaskLayout.Main
+          <Task.Main
             draggable={true}
             onClick={() => setIsModalOpen(prevState => !prevState)}
             {...provided.draggableProps}
@@ -37,23 +28,21 @@ const Task: React.FunctionComponent<Props> = props => {
             ref={provided.innerRef}
             isDragging={snapshot.isDragging}
           >
-            <TaskLayout.Time>{task.date}</TaskLayout.Time>
-            <TaskLayout.Name isDragging={snapshot.isDragging}>
-              {task.name}
-            </TaskLayout.Name>
-            <TaskLayout.Description isDragging={snapshot.isDragging}>
+            <Task.Time>{task.date}</Task.Time>
+            <Task.Name isDragging={snapshot.isDragging}>{task.name}</Task.Name>
+            <Task.Description isDragging={snapshot.isDragging}>
               <Truncate lines={4} ellipsis="..." width={210}>
                 {task.description}
               </Truncate>
-            </TaskLayout.Description>
-            <TaskLayout.Footer>
-              <TaskLayout.TagLists>
-                <TaskLayout.TagItem>
-                  <TaskLayout.Tag>UI</TaskLayout.Tag>
-                </TaskLayout.TagItem>
-              </TaskLayout.TagLists>
-            </TaskLayout.Footer>
-          </TaskLayout.Main>
+            </Task.Description>
+            <Task.Footer>
+              <Task.TagLists>
+                <Task.TagItem>
+                  <Task.Tag>UI</Task.Tag>
+                </Task.TagItem>
+              </Task.TagLists>
+            </Task.Footer>
+          </Task.Main>
         )}
       </Draggable>
       {isModalOpen && (
@@ -65,12 +54,3 @@ const Task: React.FunctionComponent<Props> = props => {
     </>
   );
 };
-
-const mapDispatchToProps: IDispatchProps = {
-  addTask
-};
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(Task);
