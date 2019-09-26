2ff962c3d3ca5f87268650d683f02c6493c570d2
Bogdan Kochurov <kochurov.b@gmail.com>
2019-09-26 18:22:05 +0400

    added types for ContextList

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -1,6 +1,16 @@
 import { createContext } from "react";
 import { Category } from "../store/types/categories";
+import { TaskType } from "../store/types/tasks";
 
 export const ContextBoard = createContext<number>(0);
-export const ContextList = createContext<any>({});
+export const ContextList = createContext<{
+  tasks: TaskType[];
+  listId: number;
+  setIsTooltipOpen: (value: boolean) => void;
+  openModalHandle?: any;
+}>({
+  setIsTooltipOpen: value => value,
+  tasks: [],
+  listId: 0
+});
 export const ContextTask = createContext<Category>({ value: "", label: "" });
